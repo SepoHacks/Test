9c340d49452543624307d0b52ca0b5913a83cb83
Sepehr Bostan <133902388+SepoHacks@users.noreply.github.com>
2024-08-22 13:54:40 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mysql = require("mysql2/promise");
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "test",
+  database: "test",
+});
+
+app.get("/db", async (req, res) => {
+  try {
+    const [result] = await db.query("SELECT 1 AS result");
+    res.json({ message: "DB connection is OK" });
+  } catch (error) {
+    console.error("Mysql error");
+    res.status(500).send("Mysql error!");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello to CI/CD" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = { port };
---
+++ tests/test.js
@@ -0,0 +1,26 @@
+const app = require('../app.js');
+
+const port = app.port || 3000;
+
+const checkServiceHealth = async (url, expectedMessage, errorMessage) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+  
+    if (data.message !== expectedMessage) {
+      throw new Error(errorMessage);
+    }
+
+    console.log(`Passed: ${expectedMessage}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+const runHealthChecks = async () => {
+  await checkServiceHealth(`http://localhost:${port}/db`, 'DB connection is OK', 'Mysql is not working');
+  await checkServiceHealth(`http://localhost:${port}/`, 'Hello to CI/CD', 'App is not working');
+};
+
+runHealthChecks();
